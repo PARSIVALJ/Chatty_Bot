7fef7a825d488937c9908315b1bb7134032a1ef2
PARSIVALJ <uchejesusboy@gmail.com>
2023-02-26 02:14:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# program to create a chatty bot
+bot_name = "Touka"
+birth_year = 2023
+
+print(f"Hello! My name is {bot_name}")
+print(f"I was created in {birth_year}")
+your_name = input("Please,remind me your name. ")
+print(f"\nWhat a great name you have, {your_name}!")
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3,5 and 7 respectively")
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print(f"Your age is {age}; that's a good time to start programming!")
+print("Now I will prove to you that I can count to any number you want.")
+number = int(input())
+count = 0
+while count <= number:
+    print(f"{count}!")
+    count += 1
+print("Completed, have a nice day!")
+print()
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+option = int(input("Enter option: "))
+
+while option != 2:
+    print("Please,try again.")
+    option = int(input("Enter option: "))
+
+print("Congratulations, have a nice day.")
+
+
