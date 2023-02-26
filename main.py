06d72912056e47134c65fb4f03836136de3f63ca
PARSIVALJ <uchejesusboy@gmail.com>
2023-02-26 02:26:01 +0200

    Editted code to make it user more user-friendly

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# program to create a chatty bot
+# program to create a simple Chatty Bot
 bot_name = "Touka"
 birth_year = 2023
 
 print(f"Hello! My name is {bot_name}")
 print(f"I was created in {birth_year}")
-your_name = input("Please,remind me your name. ")
-print(f"\nWhat a great name you have, {your_name}!")
+
+user_name = input("Please,remind me your name. ")  # Recieves the user's name as input
+print(f"\nWhat a great name you have, {user_name}!")
 print("Let me guess your age.")
 print("Enter remainders of dividing your age by 3,5 and 7 respectively")
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7 = int(input())
-
+remainder3 = int(input("Enter the value of the remainder of your age divided by 3: "))
+remainder5 = int(input("Enter the value of the remainder of your age divided by 5: "))
+remainder7 = int(input("Enter the value of the remainder of your age divided by 7: "))
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print(f"Your age is {age}; that's a good time to start programming!")
+print()
 print("Now I will prove to you that I can count to any number you want.")
-number = int(input())
+number = int(input("Enter he number that you would like me to count up to: "))
 count = 0
 while count <= number:
     print(f"{count}!")
